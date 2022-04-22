b91914ca66a7daa929b310daa9aba45163d4cd88
Jericho J <jerichojing@hotmail.com>
2022-04-21 21:14:32 -0400

    button3 added

--- button1.js
+++ button1.js
@@ -38,6 +38,26 @@ class Button2 extends React.Component {
   }
   
 }
+class Button3 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+  }
 
+  render() {
+    if (this.state.clicked == true) {
+      window.open("https://github.com/lansing213/quiz-app");
+    }
+    return (
+      <button id="button1" onClick={() => this.setState({ clicked: true })}>
+        View code
+      </button>
+      
+    );
+
+  }
+  
+}
 ReactDOM.render(React.createElement(Button1), document.querySelector('#button1_container'));
 ReactDOM.render(React.createElement(Button2), document.querySelector('#button2_container'));
+ReactDOM.render(React.createElement(Button3), document.querySelector('#button3_container'));
